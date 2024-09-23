5716631ab7acff841378d11df50a1e4036e6290e
Stefan Bethge <stefan.bethge@alcemy.tech>
2024-09-23 23:26:40 +0200

    open and copy keys from legacy location

--- SparkleShare-Mac/SSHAuthenticationInfo.swift
+++ SparkleShare-Mac/SSHAuthenticationInfo.swift
@@ -26,18 +26,30 @@ class SSHAuthenticationInfo {
         self.knownHostsFilePath = configurationPath.appendingPathComponent("known_hosts").path
         SSHAuthenticationInfo.createDirectoryIfNeeded(at: configurationPath)
 
-        if let keys = SSHAuthenticationInfo.importKeys(from: self.path) {
-            self.privateKeyFilePath = keys.privateKeyFilePath
-            self.privateKey = keys.privateKey
-            self.publicKeyFilePath = keys.publicKeyFilePath
-            self.publicKey = keys.publicKey
-        } else {
-            let keys = SSHAuthenticationInfo.createKeyPair(at: self.path)
-            self.privateKeyFilePath = keys.privateKeyFilePath
-            self.privateKey = keys.privateKey
-            self.publicKeyFilePath = keys.publicKeyFilePath
-            self.publicKey = keys.publicKey
+        func assignKeys(_ keys: (privateKeyFilePath: String, privateKey: String, publicKeyFilePath: String, publicKey: String)) {
         }
+
+        let keys: (privateKeyFilePath: String, privateKey: String, publicKeyFilePath: String, publicKey: String)
+        if let localKeys = SSHAuthenticationInfo.importKeys(from: self.path) {
+            keys = localKeys
+        } else {
+            // Before creating new files, also check the location of C# SparkleShare for files
+            let legacyConfigurationPath = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent(".config/org.sparkleshare.SparkleShare/ssh").path
+            if let legacyKeys = SSHAuthenticationInfo.importKeys(from: legacyConfigurationPath) {
+                print("Using legacy ssh keys from \(legacyConfigurationPath).")
+                keys = legacyKeys
+                //TODO: copy legacy keys to new location
+                SSHAuthenticationInfo.copyLegacyKeys(to: self.path)
+            } else {
+                print("No ssh keys found, creating some...")
+                keys = SSHAuthenticationInfo.createKeyPair(at: self.path)
+            }
+        }
+        
+        self.privateKeyFilePath = keys.privateKeyFilePath
+        self.privateKey = keys.privateKey
+        self.publicKeyFilePath = keys.publicKeyFilePath
+        self.publicKey = keys.publicKey
     }
 
     private static func createDirectoryIfNeeded(at url: URL) {
@@ -66,6 +78,29 @@ class SSHAuthenticationInfo {
             print("Error importing keys: \(error)")
         }
         return nil
+    }
+    
+    private static func copyLegacyKeys(to path: String) {
+        let fileManager = FileManager.default
+        let legacyConfigurationPath = fileManager.homeDirectoryForCurrentUser.appendingPathComponent(".config/org.sparkleshare.SparkleShare/ssh").path
+        
+        do {
+            let legacyFiles = try fileManager.contentsOfDirectory(atPath: legacyConfigurationPath)
+            // Iterate through each file in the legacy directory
+            for file in legacyFiles {
+                let legacyFilePath = (legacyConfigurationPath as NSString).appendingPathComponent(file)
+                let newFilePath = (path as NSString).appendingPathComponent(file)
+                
+                if !fileManager.fileExists(atPath: newFilePath) {
+                    try fileManager.copyItem(atPath: legacyFilePath, toPath: newFilePath)
+                    print("Copied \(file) to new location")
+                } else {
+                    print("\(file) already exists at the new location")
+                }
+            }
+        } catch {
+            print("Error copying legacy keys: \(error.localizedDescription)")
+        }
     }
 
     private static func createKeyPair(at path: String) -> (privateKeyFilePath: String, privateKey: String, publicKeyFilePath: String, publicKey: String) {
